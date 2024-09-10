e2124fe5059c91e9173d73b7bd8ba0149238b802
henrico cesar <henricooleao@gmail.com>
2024-09-10 20:36:31 -0300

    adicionar inquirer

---
+++ index3.js
@@ -0,0 +1,17 @@
+const start = () => {
+
+    while(true){
+        switch(opecao) {
+            case "cadastrar":
+                console.log("vamos cadastrar")
+                break
+            case "vamos listar":  
+                console.log("vamos listar")
+                break
+            case "sair":
+                return  
+        }
+    }
+}
+
+start()
