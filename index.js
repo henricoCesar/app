46a7b84c6bad0944e10c79157c2b2442d1c6a472
henrico cesar <henricooleao@gmail.com>
2024-09-10 13:15:34 -0300

    function

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-// ola mundo
-console.log("ola mundo")
+// arrys objetos
+let meta = {
+    value: "ler um livro",
+    checked: false,
+    log: (info) => {
+        console.log(info)
+    }
+}
 
+meta.value = "não e mais ler livro"
+meta.log(meta.value)
+
+// arrow function
+const criaMetas = () => {}
+// function 
+function criarMetas () {}
 
