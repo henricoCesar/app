a8b38305b15678d0108fe0839efdfbb86bd0a96a
henrico cesar <henricooleao@gmail.com>
2024-09-10 13:28:16 -0300

    metas,value

---
+++ index2.js
@@ -0,0 +1,13 @@
+let meta = {
+    value: "ler um livro",
+    checked: true,
+}
+
+let metas = [
+    meta,
+    {
+        value: "caminhar 20 minutos do o dias",
+        checked: false,
+    }
+]
+console.log(metas[0].value)
